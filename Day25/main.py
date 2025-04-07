9b6ac524398e0e53c9c7b29227502836daf14f1e
OgnyanIgnatov <ognyanign@gmail.com>
2025-04-08 01:01:55 +0300

    minor day 25

--- Day25/main.py
+++ Day25/main.py
@@ -10,4 +10,18 @@ import csv, pandas
 #     print(temperatures)
 
 data = pandas.read_csv("./Day25/weather_data.csv")
-print(data["temp"])
+
+# temps = data["temp"].to_list()
+# average_temp = 0
+# for temp in temps:
+#     average_temp += temp
+# average_temp/=len(temps)
+# print(f"{average_temp:.2f}")
+
+# max_temp = data["temp"].max()
+# print(data[data.temp == max_temp])
+# print(max_temp)
+
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0]
+print(monday_temp*9/5 +32)
