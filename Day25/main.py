d901b30ab5247c451a606e7bd47d7cb9b12f4f68
OgnyanIgnatov <ognyanign@gmail.com>
2025-04-09 00:58:13 +0300

    Central Park related survey

--- Day25/main.py
+++ Day25/main.py
@@ -9,7 +9,7 @@ import csv, pandas
 #         temperatures.append(int(row[1]))
 #     print(temperatures)
 
-data = pandas.read_csv("./Day25/weather_data.csv")
+# data = pandas.read_csv("./Day25/weather_data.csv")
 
 # temps = data["temp"].to_list()
 # average_temp = 0
@@ -22,6 +22,21 @@ data = pandas.read_csv("./Day25/weather_data.csv")
 # print(data[data.temp == max_temp])
 # print(max_temp)
 
-monday = data[data.day == "Monday"]
-monday_temp = monday.temp[0]
-print(monday_temp*9/5 +32)
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# print(monday_temp*9/5 +32)
+
+
+data = pandas.read_csv("./100-Days-of-Code-Bootcamp/Day25/squirls.csv")
+
+gray_squirls_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirls_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirls_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Grey", "Cinnamon", "Black"],
+    "Count" : [gray_squirls_count, red_squirls_count, black_squirls_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("./100-Days-of-Code-Bootcamp/Day25/new_squirls.csv")
